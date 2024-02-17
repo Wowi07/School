c6d3d4faea2e19c62e097ec8f5ee7a7ac071823c
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-02-17 15:16:39 +1300

    Update game_play.py

--- game_play.py
+++ game_play.py
@@ -1,9 +1,12 @@
 def game_play():
   s1=randnum()
   s2=randnum()
+  dbscore=False
+  if s1==s2:
+    dbscore=True
   sum=s1+s2
   while True:
     if(yes_no("Do you want to continue rolling the dice? "):
       sum=sum+randnum()
     else:
-      return sum
+      return sum , dbscore
