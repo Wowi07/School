5fff0f7f6d84fb174f975da05442da5fd53a285f
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-02-17 15:33:33 +1300

    Update game_play.py

--- game_play.py
+++ game_play.py
@@ -5,11 +5,12 @@ def game_play():
   if s1==s2:
     dbscore=True
   sum=s1+s2
+  print(f"You rolled {s1} and {s2}, your total score is {sum}")
   while True:
     if(yes_no("Do you want to continue rolling the dice? ")):
       temp=randnum()
       sum=sum+temp
-      print(f"You rolled {temp}"+"\n"+f"Your total score is now {sum}")
+      print("\n"+"\n"+f"You rolled {temp}"+"\n"+f"Your total score is now {sum}")
     else:
       return sum , dbscore
 playerscore , db = game_play()
