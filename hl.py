69b13842abf4b6a0d8168606afc83cd842db496a
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-03-06 14:29:44 +1300

    Create hl.py

---
+++ hl.py
@@ -0,0 +1,11 @@
+def instruction():
+  print('''    👆👆👆Higher Lower Game👇👇👇  
+  - You can either choose the number of rounds or they can opt for infinite mode(by pressing 0).
+  - You can choose the parameters for the game (ie: they choose the range involved which means they need to give us a low number and a high number where the mystery number will be between the two numbers we are given at the start of the game).
+  - If you press the exit code ‘xxx’ or play the requested number of rounds, then the game end.
+  - When the game ends, you are able to choose to see your game history.
+  -There is a 3 different results in a single round, which is gold, silver or bronze depends on your number of guesses.
+  -You will have an initial score for each round, which depend on the range you chose.(example: If you choose the range [1,100], your initial score gonna be 100. 
+  -Each time you guess wrong, your score will be minus by 1
+  ***🍀🍀 Good Luck 🍀🍀***
+  ''')
