1d444ef54195f45f0c8729fcb9e61b6af3a41375
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-03-08 14:05:53 +1300

    Update balloon.py

--- balloon.py
+++ balloon.py
@@ -3,8 +3,8 @@ import random
 def instruction():
   print('''      DON'T POP THE BALLOON🎈
   + Player can choose how many rounds they want to play, or join the infinite mode by enter 0
-  + Your point gain per round depends on your streak, many streak ur having, many points you gain on that round( If your streak is 5, if you win that round, you will get 6 points)
-  + The game start, you can choose between 1 to 3. These number stand for the times you blow the ballon...
+  + Your point gain per round depends on your streak, many streak ur having, many points you gain on that round( If your streak is 5, and if you win that round, you will get 6 points)
+  + When the game starts, you can choose a number between 1 to 3. These numbers stand for the times you blow the ballon...
   + Don't blow too much or your balloon will pop. Huy(npc) will play with you, be careful from him, he talk too much tho...
   ''')
 def yes_no(question):
@@ -45,4 +45,19 @@ def game_round():
         
 #main 
 if yes_no("Do you want to read the instruction")
+  instruction()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
