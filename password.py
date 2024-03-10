044b045820c0644fe8c717b1e1efa09fffcc856e
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-03-11 11:03:43 +1300

    Update password.py

--- password.py
+++ password.py
@@ -5,7 +5,7 @@ def instruction():
 -Huy created his laptop's password and made it changes everyday. 
 -His first time of changing password is in January 1st 2021.
 -The password today is the sum of a list of number from 0 to n, with n is the day since his first time of changing password.(if n=2, today is 3rd Jan and the password is 3)
--The program will display a random day from Jan 1st 2021 to Jan 1st 2024
+-The program will display a random day before 1st Jan 2024
 -Your mission is input the password of his laptop on that day, goodluck....
 '')
 # Sum of range 
