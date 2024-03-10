4fefbe52e6c280c7fd03dae0f601b4d1287e95f4
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-03-11 10:42:34 +1300

    Create password.py

---
+++ password.py
@@ -0,0 +1,12 @@
+def instruction():
+    print("Password💻🔑")
+    print('''This program can generate the quiz 
+        This is the problem...
+-Huy created his laptop's password and made it changes everyday. 
+-His first time of changing password is in January 1st 2021.
+-The password today is the sum of a list of number from 0 to n, with n is the day since his first time of changing password.(if n=2, today is 3rd Jan and the password is 3)
+-The program will display a random day from Jan 1st 2021 to Jan 1st 2024
+-Your mission is input the password of his laptop on that day, goodluck....
+'')
+# Sum of range 
+print("Password💻🔑")
