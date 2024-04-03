0c73781c4a8b2d5daf73ad39ecced46ab93bf454
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-04-03 13:40:01 +1300

    Update who_want_candies.py

--- who_want_candies.py
+++ who_want_candies.py
@@ -5,7 +5,8 @@ def instruction():
     * Easy  (|a|,|b|<=10).
     * Normal(|a|,|b|<=1000).
     * Hard  (|a|,|b|<=10000000).
-- With the divide quiz, you must round the answer to the nearest interger number.(eg: 1.98 will be 2, 0.12 will be 0).
+- With the divide quiz, you must round the answer to the nearest interger number(eg: 1.98 will be 2, 0.12 will be 0), if there is x.5(like 1.5 or 7.5) 
+you must round to the nearest even number(if there is 1.5, round to 2; 7.5 round to 8, 10.5 round to 10).
 - Every correct answer, you will have 1 candy(Easy), 2 candies(Normal) and 3 candies(Hard).
          ~~ Don't make grandma sad.😉
     """)
