03d77f4bcd220df71b41a0798dcaa26322dce17b
TruongNguyenGiaHuy <159389006+TruongNguyenGiaHuy@users.noreply.github.com>
2024-05-19 00:10:41 +1200

    Update who_want_candies.py

--- who_want_candies.py
+++ who_want_candies.py
@@ -1,5 +1,5 @@
 def instructions():
-    print("""     Who want candies? 🍬🍭
+    print("""
 - Grandma want to test your calculate skill by asking tons of quiz about plus(+), minus(-), multiply(x) or divide(/) 2 numbers 'a' and 'b' 
 (a+b,a-b,a x b or a/b).
 - Before she give you a quiz, you can choose level of a quiz which is: 
@@ -7,14 +7,13 @@ def instructions():
     * 🟨 Normal(|a|,|b|<=1000).
     * 🟥 Hard  (|a|,|b|<=10000000).
 - In any level, you still have a chance to get an easy question even in hard mode.
-- With the divide quiz, you must round the answer to the nearest integer number(eg: 1.98 will be 2, 0.12 will be 0), if there is x.5(like 1.5 or 7.5) 
+- With the divide quiz, you must question the answer to the nearest integer number(eg: 1.98 will be 2, 0.12 will be 0), if there is x.5(like 1.5 or 7.5) 
 you must round to the nearest even number(if there is 1.5, round to 2; 7.5 round to 8, 10.5 round to 10).
 - Every correct answer, you will have 1 candy(Easy), 2 candies(Normal) and 3 candies(Hard).
 - You will have 3 times to answer each grandma's question.
 - If you need a break, just say “I wanna sleep grandma” to end the game.(She won’t let you leave if you are not saying a correct sentence).
              ~~ Don't make grandma sad.😉
     """)
-# This function return yes or no only depends on player response
 def yes_no(question):
     print(question)
     error="You did not choose a valid response"
@@ -29,9 +28,8 @@ def yes_no(question):
                 print(error) 
         except ValueError:
             print(error)
-# I use this one when I want to know how many rounds player wanna play
-def how_many_rounds():
-    print("How many rounds you want to play?( input 0 to start an infinite mode ♾️  ...)")
+def how_many_questions():
+    print("How many questions you want to answer?( input 0 to start an infinite mode ♾️  ...)")
     error = "Please input an integer greater than 0 or input 0 to start an infinite mode ♾️  ..."
     while True:
         try:
@@ -49,7 +47,6 @@ def how_many_rounds():
                 return ans
         except ValueError:
             print(error)
-# change number to operator
 def change_symbol(s):
     if s==1:
         return "+"
@@ -59,7 +56,6 @@ def change_symbol(s):
         return "x"
     if s==4:
         return "/"
-# This function will return a level player chose
 def level_choosing():
     print("""Please choose the following level by input the first letter or full word.
     * 🟩 Easy  (|a|,|b|<=10).
@@ -73,15 +69,14 @@ def level_choosing():
             return "exit"
         if response=="easy" or response=="e":
             print("You choose an Easy quiz🟩")
-            return 10,1,"You get 1 candy from this round 🟩"
+            return 10,1,"You get 1 candy from this question 🟩"
         if response=="normal" or response=="n":
             print("You choose a Normal quiz🟨")
-            return 1000,2 ,"You get 2 candies from this round 🟨"
+            return 1000,2 ,"You get 2 candies from this question 🟨"
         if response=="hard" or response=="h":
             print("You choose a Hard quiz🟥")
-            return 10000000,3,"You get 3 candies from this round 🟥"
+            return 10000000,3,"You get 3 candies from this question 🟥"
         print(error)
-# This function return the answer of quiz
 def calculate_the_answer(a,b,operator):
     if operator==1:
         return a+b
@@ -94,22 +89,17 @@ def calculate_the_answer(a,b,operator):
 #history variables
 quiz_content=[]
 result_history=[]
-turn_used_that_round=[]
+turn_used_that_question=[]
 import random
-# single round here
-def single_round(boundary):
+def single_quiz(boundary):
     #create quiz
     symbol=random.randint(1,4)
-    #random.randint(boundary*(-1),boundary) mean i will random a number from -boundary to boundary
-    # eg: if my boundary is 10, so i will random a number from -10 to 10
     a=random.randint(boundary*(-1),boundary)
     b=random.randint(boundary*(-1),boundary)
     print(f"What is the answer of {a} {change_symbol(symbol)} {b}")
     quiz_content.append(f"What is the answer of {a} {change_symbol(symbol)} {b}")
     correct_answer=calculate_the_answer(a,b,symbol)
-    # duplicated_answer is an array created to save player's answers
     duplicated_answer=[]
-    # this will loop 3 times stand for 3 times answer the question
     for i in range(1,4):
         print(f"You have {3-i+1} turn(s) left to answer...")
         while True:
@@ -126,83 +116,74 @@ def single_round(boundary):
             except ValueError:
                 print("You did not choose a valid response")
         if player_answer==correct_answer:
-            result_history.append(f"You gave a correct answer in this round which is {correct_answer}...")
-            turn_used_that_round.append(i)
+            result_history.append(f"You gave a correct answer in this question which is {correct_answer}...")
+            turn_used_that_question.append(i)
             return True,i
-        #add the player_answer into duplicated_answer to mark that they have alr inputted this answer
         duplicated_answer.append(player_answer)
     print(f"The correct answer is {correct_answer}")
-    result_history.append(f"You didn't give any correct answer in this round, the answer is {correct_answer}...")
-    turn_used_that_round.append(i)
+    result_history.append(f"You didn't give any correct answer in this question, the answer is {correct_answer}...")
+    turn_used_that_question.append(i)
     return False,3
 
 #main 
 #statistics variable
-total_win_round=0
+total_correct_question=0
 total_guesses=0
 player_total_candy=0
+print("     Who want candies? 🍬🍭")
 if yes_no("Do you want to read the instructions?  "):
     instructions()
-win_speech="🎉 Haaaa, grandma is proud of you 🎉"
-lose_speech="😒 Grandma is kinda disappointed about you 🤔"
+correct_speech="🎉 Haaaa, grandma is proud of you 🎉"
+incorrect_speech="😒 Grandma is kinda disappointed about you 🤔"
 exit_sign=True
-rounds_left=how_many_rounds()
-# in how_many_rounds() function, i will return "exit" if player input an exit code
-# so if they wanna exit, i will change the exit_sign from True to False to mark that
-# they wanted to exit
-if rounds_left=="exit":
+questions_left=how_many_questions()
+if questions_left=="exit":
     print("👵 Ok sweetie...")
     exit_sign=False
-# infinite_mode created because i wanna show "  ♾️  Infinite mode ♾️" if player chose inf mode
 infinite_mode=""
-if rounds_left==-1:
+if questions_left==-1:
     infinite_mode="  ♾️  Infinite mode ♾️"
-current_round=1
+question_answered=1
 if exit_sign:
-    while rounds_left!=0:
+    #Game runs if player did not exit yet
+    while questions_left!=0:
         print("\n"*3)
-        print(f"Round {current_round}"+infinite_mode)
-        #level will contain 
-        #level[0] : a boundary to random
-        #level[1] : how manycandy player can get if they win 
-        #level[2]:  winning speech
+        print(f"Question {question_answered}? "+infinite_mode)
         level=level_choosing()
-        #once again, my level_choosing() will return "exit" if player inputted an exit code
         if level=="exit":
             print("👵 Ok sweetie...")
             break
-        # single_round_result is a variable which contains True or False stand for win or lose, and how many turn(s) they used
-        single_round_result=single_round(level[0])
-        if single_round_result=="exit":
+        single_quiz_result=single_quiz(level[0])
+        if single_quiz_result=="exit":
             print("👵 Ok sweetie...")
             break
-        elif single_round_result[0]:
-            total_win_round=total_win_round+1
-            total_guesses=total_guesses+single_round_result[1]
-            print(win_speech)
+        elif single_quiz_result[0]:
+            total_correct_question=total_correct_question+1
+            total_guesses=total_guesses+single_quiz_result[1]
+            print(correct_speech)
             player_total_candy=player_total_candy+level[1]
             print(level[2])
             print(f"Your total candies is {player_total_candy}...")
         else:
-            print(lose_speech)
-            print(f"You don't get any candy in this round, so your total candy is still {player_total_candy}...")
+            print(incorrect_speech)
+            print(f"You don't get any candy in this question, so your total candy is still {player_total_candy}...")
             total_guesses=total_guesses+3
-        rounds_left=rounds_left-1
-        current_round=current_round+1
-if current_round==1:
-    print("You did not played any round...")
+        questions_left=questions_left-1
+        question_answered=question_answered+1
+if question_answered==1:
+    print("You did not played any question...")
 else:
     if yes_no("Do you want to see the history?  "):
         print("     🕰️ History 🕰️")
-        for i in range(1,current_round):
-            print(f"    Round {i}:   ")
+        for i in range(1,question_answered):
+            print(f"    question {i}:   ")
             print(f"Quiz:   ",quiz_content[i-1],"?")
-            print(f"({turn_used_that_round[i-1]} turn(s) used)",result_history[i-1])
+            print(f"(You used {turn_used_that_question[i-1]} time(s) to answer)",result_history[i-1])
     #statistics
     print("     📊 Statistics 📊")
-    print(f"You played {current_round-1} round(s)")
-    print(f"Total rounds you gave a correct answer is {total_win_round} round(s)  ({(total_win_round*100)/(current_round-1)}%)")
-    print(f"You used about {int((total_guesses)/(current_round-1))} turn(s) per round. ")
+    print(f"You played {question_answered-1} question(s)")
+    print(f"Total questions you gave a correct answer is {total_correct_question} question(s)  ({(total_correct_question*100)/(question_answered-1)}%)")
+    print(f"You used about {int((total_guesses)/(question_answered-1))} time(s) to answer per question. ")
 
 
 
